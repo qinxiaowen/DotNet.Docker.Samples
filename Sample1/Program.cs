744b623b5a06038dbba128bce8616c67b89f146d
qinxiaowen <453000697@qq.com>
2018-11-27 21:05:47 +0800

    1

--- Sample1/Program.cs
+++ Sample1/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using Docker.DotNet;
 using Docker.DotNet.Models;
+using Newtonsoft.Json;
 
 namespace Sample1
 {
@@ -44,26 +46,121 @@ namespace Sample1
             }
 
 
-            //docker pull
+            ////docker pull
 
-            client.Images.CreateImageAsync(new Docker.DotNet.Models.ImagesCreateParameters()
+            //client.Images.CreateImageAsync(new Docker.DotNet.Models.ImagesCreateParameters()
+            //{
+            //    FromImage = "mysql",
+            //    Tag = "5.5",
+            //},
+            //new AuthConfig()
+            //{
+
+            //},
+            //new Progress()).Wait();
+
+
+            //docker create container 
+            // docker create
+            //var createContainerResponse = client.Containers.CreateContainerAsync(new CreateContainerParameters()
+            //{
+            //    Image = "nginx:latest",
+            //    Name = "nginx-01",
+            //     //Cmd=new string[] {"" }
+            //}).Result;
+            client.Containers.StartContainerAsync("943b9ef8ac03", new ContainerStartParameters());
+
+
+            //Config createContainerParameters = new Config
+            //{
+            //    Image = "mysql:5.5",
+            //    ArgsEscaped = false,
+            //    AttachStderr = false,
+            //    AttachStdin = true,
+            //    AttachStdout = true,
+            //    Cmd = new string[] {
+            //         "--name","some-mysql2",
+            //            "-e","MYSQL_ROOT_PASSWORD=my-secret-pw",
+            //            "-d","mysql:5.5"
+            //    },
+
+            //    OpenStdin = true,
+            //    StdinOnce = true,
+            //};
+
+            //var createContainerResponse = client.Containers.CreateContainerAsync(new CreateContainerParameters(createContainerParameters)).Result;
+
+
+            string mContainerPort = "8080";
+            string mHostPort = "8081";
+
+            var createR = client.Containers.CreateContainerAsync(new CreateContainerParameters()
             {
-                FromImage = "hello-world",
-                Tag = "latest",
-            },
-new AuthConfig(),
-new Progress()).Wait();
-
-
-            //docker pull
-            var createContainerResponse = client.Containers.CreateContainerAsync(new CreateContainerParameters()
-            {
-                Image = "hello-world",
-                Name = "hello-world-01",
-
+                Image = "microsoft/dotnet-samples:aspnetapp",
+                Name = "aspnetcore_sample7",
+                ExposedPorts = new Dictionary<string, object> {
+        {
+            mContainerPort, new {
+                HostPort = mHostPort
+            }
+        }
+    },
+                HostConfig = new HostConfig()
+                {
+                    PublishAllPorts = true,
+                    PortBindings = new Dictionary<string, IList<PortBinding>>
+                    {
+                        { "80/tcp",
+                            new List<PortBinding> {
+                                new PortBinding {
+                                    HostIP="0.0.0.0",
+                                    HostPort ="8000"
+                                }
+                            }
+                        }
+                    }
+                }
             }).Result;
 
-            client.Containers.
+
+            //docker start
+            //943b9ef8ac03
+            //var r1 = client.Containers.StartContainerAsync("82e0fb93b86a", new ContainerStartParameters()
+            //{
+            //    DetachKeys = "123123123123123123"
+            //}).Result;
+
+
+
+            //var createContainerResponse = client.Containers.StartWithConfigContainerExecAsync("0c460e08b817", new ContainerExecStartParameters()
+            //{
+            //    AttachStderr = true,
+            //    AttachStdin = true,
+            //    AttachStdout = true,
+            //    Cmd = new string[] { "env", "TERM=xterm-256color", "bash" },
+            //    Detach = false,
+            //    Tty = false,
+            //    User = "root",
+            //    Privileged = true,
+
+
+            //    Cmd = new string[] {
+            //          "--name","some-mysql2",
+            //          "-e","MYSQL_ROOT_PASSWORD=my-secret-pw",
+            //          "-d","mysql:5.5"
+            //      }
+            //}).Result;
+
+
+            ////docker pull
+            //var createContainerResponse = client.Containers.CreateContainerAsync(new CreateContainerParameters()
+            //{
+            //    Image = "hello-world",
+            //    Name = "hello-world-01",
+
+            //}).Result;
+
+            //var processes = client.Containers.ListProcessesAsync();
 
 
 
@@ -87,7 +184,7 @@ new Progress()).Wait();
         {
             public void Report(JSONMessage value)
             {
-                Console.WriteLine(value.ToString());
+                Console.WriteLine(JsonConvert.SerializeObject(value));
             }
         }
     }
